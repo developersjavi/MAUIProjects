20409bfb979d28323ddec6ca4affe23cc67b7e7c
J. Jackson Moura <developersjavi@gmail.com>
2025-07-05 22:41:38 -0300

    Unload Project

---
+++ MauiAppContentPage/MainPage.xaml.cs
@@ -0,0 +1,9 @@
+namespace MauiAppContentPage;
+
+public partial class MainPage : ContentPage
+{
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+}
---
+++ MauiAppContentPage/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace MauiAppContentPage
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
